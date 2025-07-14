d28e02c4c16c6d087c409e76ca60506518b9a7c6
vlfwww <vlfwww@outlook.com>
2025-07-14 15:34:03 +0300

    added new section

--- app/src/app/page.js
+++ app/src/app/page.js
@@ -10,6 +10,16 @@ import catalog3 from "../../public/catalog3.png";
 import catalog4 from "../../public/catalog4.png";
 import catalog5 from "../../public/catalog5.png";
 import catalog6 from "../../public/catalog6.png";
+import about from '../../public/about.png';
+import about1 from '../../public/about1.png';
+import about2 from '../../public/about2.png';
+import about3 from '../../public/about3.png';
+import about4 from '../../public/about4.png';
+import bike from '../../public/bike.png';
+import phone from '../../public/phone.png';
+import chain from '../../public/chain.png';
+import gear from '../../public/gear.png';
+import pic from '../../public/pic.png'
 
 const arr = [partner, partner, partner, partner, partner, partner, partner];
 
@@ -38,49 +48,160 @@ export default function Home() {
         </div>
       </div>
       <section className={style.partners}>
-        {arr.map((el) => (
-          <Image src={el} alt="img" />
+        {arr.map((el, index) => (
+          <Image key={`partner-${index}`} src={el} alt="partner logo" />
         ))}
       </section>
       <section className={style.sale}>
         <p className={style.title}>Новинки</p>
         <div className={style.wrapper}>
-          {bikes.map((el) => (
-            <SaleBlock img={el.img} title={el.title} price={el.price} />
+          {bikes.map((el, index) => (
+            <SaleBlock
+              key={el.id || index}
+              img={el.img}
+              title={el.title}
+              price={el.price}
+            />
           ))}
         </div>
         <p className={style.showAll}>показать все</p>
       </section>
       <section className={style.catalog}>
         <div className={style.catalogWrapper}>
-          <p className={style.title}>Каталог</p>
+          <h2 className={style.title}>КАТАЛОГ</h2>
           <div className={style.wrapper}>
-            <div className={style.block}>
-              <p>Велосипеды</p>
-              <Image src={catalog1} />
+            <div className={`${style.block} ${style.bikes}`}>
+              <p>ВЕЛОСИПЕДЫ</p>
+              <Image src={catalog1} alt="Велосипеды" className={style.image} />
             </div>
-            <div className={style.block}>
-              <p>Trade-in</p>
-              <Image src={catalog2} />
+            <div className={`${style.block} ${style.tradeIn}`}>
+              <p>TRADE-IN</p>
+              <Image src={catalog2} alt="Trade-in" className={style.image} />
             </div>
-            <div className={style.block}>
-              <p>Запчасти</p>
-              <Image src={catalog3} />
+
+            <div className={`${style.block} ${style.parts}`}>
+              <p>ЗАПЧАСТИ</p>
+              <Image src={catalog3} alt="Запчасти" className={style.image} />
             </div>
-            <div className={style.block}>
-              <p>Аксессуары</p>
-              <Image src={catalog4} />
+
+            <div className={`${style.block} ${style.equpm}`}>
+              <p>ЭКИПИРОВКА</p>
+              <Image src={catalog5} alt="Экипировка" className={style.image} />
             </div>
-            <div className={style.block}>
-              <p>Экипировка</p>
-              <Image src={catalog5} />
+
+            <div className={`${style.block} ${style.acses}`}>
+              <p>АКСЕССУАРЫ</p>
+              <Image src={catalog4} alt="Аксессуары" className={style.image} />
             </div>
-            <div className={style.block}>
-              <p>Велостанки</p>
-              <Image src={catalog6} />
+
+            <div className={`${style.block} ${style.parts}`}>
+              <p>ВЕЛОСТАНКИ</p>
+              <Image src={catalog6} alt="Велостанки" className={style.image} />
             </div>
           </div>
         </div>
+        <div className={style.addition}>
+          <p className={style.title}>Ничего не сможет остановить вас</p>
+          <div className={style.right}>
+            <p className={style.main}>
+              Наша компания специализируется на продаже товаров
+              для велосипедного спорта — велосипедов, запасных частей,
+              аксессуаров и различного спортивного инвентаря для активного
+              спорта и отдыха.
+            </p>
+            <p className={style.button}>Подробнее</p>
+          </div>
+        </div>
+      </section>
+      <section className={style.about}>
+        <Image src={about} alt="img" className={style.mainImg}/>
+        <div className={style.aboutWrapper}>
+          <div className={style.block}>
+            <div className={style.wrapper}>
+              <Image src={about1} alt="img" />
+              <p>Европейские бренды</p>
+            </div>
+            <p className={style.descr}>Представляем десятки европейских брендов</p>
+          </div>
+          <div className={style.block}>
+            <div className={style.wrapper}>
+              <Image src={about2} alt="img" />
+              <p>Вечная гарантия</p>
+            </div>
+            <p  className={style.descr}>На некоторые бренды предоставляем пожизненную гарантию</p>
+          </div>
+          <div className={style.block}>
+            <div className={style.wrapper}>
+              <Image src={about3} alt="img" />
+              <p>Предпродажная настройка</p>
+            </div>
+            <p  className={style.descr}>Специалисты настроят ваш велосипед наилучшим образом</p>
+          </div>
+          <div className={style.block}>
+            <div className={style.wrapper}>
+              <Image src={about4} alt="img" />
+              <p>Доставка за 24 часа</p>
+            </div>
+            <p  className={style.descr}>Доставляем товар всеми популярными транспортными компаниями</p>
+          </div>
+        </div>
+      </section>
+      <section className={style.models}>
+        <div className={style.modelWrapper}>
+          <p className={style.title}>Лучшие модели для зимней езды</p>
+          <div className={style.wrapper}>
+          {bikes.map((el, index) => (
+            <SaleBlock
+              key={el.id || index}
+              img={el.img}
+              title={el.title}
+              price={el.price}
+            />
+          ))}
+        </div>
+        </div>
+      </section>
+      <section className={style.mountain}>
+          <div className={style.mountainWrapper}>
+            <p className={style.title}>Горные велосипеды</p>
+            <div className={style.wrapper}>
+                <div className={style.left}>
+                  <div className={style.block}>
+                    <div className={style.flex}>
+                      <p>Рама</p>
+                      <Image src={bike} alt="img"/>
+                    </div>
+                    <p className={style.descr}>It is a long established fact that a reader will be distracted by the readable content of</p>
+                  </div>
+                  <div className={style.block}>
+                    <div className={style.flex}>
+                      <p>Бортовой компьютер</p>
+                      <Image src={phone} alt="img"/>
+                    </div>
+                    <p className={style.descr}>The point of using lorem ipsum is that it has a more-or-less normal distribution of letters</p>
+                  </div>
+                  <div className={style.block}>
+                    <div className={style.flex}>
+                      <p>Трансмиссия</p>
+                      <Image src={chain} alt="img"/>
+                    </div>
+                    <p className={style.descr}>Many desktop publishing packages and web page editors now use lorem ipsum as </p>
+                  </div>
+                  <div className={style.block}>
+                    <div className={style.flex}>
+                      <p>Оборудование</p>
+                      <Image src={gear} alt="img"/>
+                    </div>
+                    <p className={style.descr}>Contrary to popular belief, lorem ipsum is not simply random text. It has roots in</p>
+                  </div>
+                </div>
+                <div className={style.right}>
+                  <Image src={pic} alt="img"/>
+                  <p>Горный велосипед несмотря на свое название часто и активно используются в условиях города, так как обладает рядом характеристик, позволяющих сделать велопрогулку максимально приятной и комфортной</p>
+                  <button>Каталог</button>
+                </div>
+            </div>
+          </div>
       </section>
     </div>
   );
